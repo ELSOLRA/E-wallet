fd35108fc45540351c4794514332cec5e177e0bb
ELSOLRA <bardakasmiljo@gmail.com>
2024-02-16 22:40:20 +0100

    putting components assets inside

---
+++ src/components/UI/Logo.jsx
@@ -0,0 +1,11 @@
+import Bitcoinlogo from '../../assets/icons/Bitcoinlogo.svg'
+
+
+
+const AppIcon = () => {
+    return (
+        <img src={Bitcoinlogo}></img>
+    )
+}
+
+export default AppIcon;
