5da03adc555f80203da2bba0e0ad072db6863d53
ELSOLRA <bardakasmiljo@gmail.com>
2024-02-19 03:00:50 +0100

    added more control to input type, content and size, converted input data types to store in localStorage

--- src/components/cardForm/CardForm.tsx
+++ src/components/cardForm/CardForm.tsx
@@ -7,7 +7,7 @@ type FormData = {
   cardnumber: string;
   cardHolderName: string;
   validThru: string;
-  ccv: number | null;
+  ccv: string;
 };
 
 type Props = {};
@@ -21,14 +21,15 @@ const CardForm: React.FC<Props> = (props: Props) => {
       cardnumber: "", 
       cardHolderName: "", 
       validThru:"",
-      ccv:null,
+      ccv:"",
     
     });
 
-    const [ccvError, setCcvError] = useState<string | null>(null);
+    // const [ccvError, setCcvError] = useState<string | null>(null);
     const [formId, setFormId] = useState<number>(1);
     const [submittedForms, setSubmittedForms] = useState<FormData[]>([]);
     const [duplicatedFormData, setDuplicatedFormData] = useState<FormData | null>(null);
+    const [formErrors, setFormErrors] = useState<Record<string, string>>({});
     /* const [submitted, setSubmitted] = useState<boolean>(false); */
 
     useEffect(() => {
@@ -45,42 +46,85 @@ const CardForm: React.FC<Props> = (props: Props) => {
       }
     }, [formId]);
 
+    
+    
     function handleChange(event: ChangeEvent<HTMLInputElement>): void {
 
       const { name, value } = event.target;
+      
+      
+     if (name === 'cardnumber') {
+      
+      const formattedCardNumber = value.replace(/\D/g, '');  // we can use .slice(0, 16) if we don't have maxLength={16} on input   
+      setFormData((prevFormData: FormData) => ({ 
+        ...prevFormData,
+        [name]: formattedCardNumber 
+      }));
+      setDuplicatedFormData((prevDuplicatedFormData: FormData | null) => ({
+        ...prevDuplicatedFormData!,
+        [name]: formattedCardNumber.replace(/(.{4})/g, '$1 '),
+      }));
+      
+    } else if (name === 'cardHolderName' ) {
 
-      if (name === 'ccv') {
-        if (/^\d+$/.test(value)) {
-          const numericValue = Number(value) ; //  or parseInt(value, 10)
-          setFormData((prevFormData) => ({
-            ...prevFormData,
-            [name]: numericValue,
-          }));
-          setCcvError(null); // Clear error 
-        } else {
-          setFormData((prevFormData) => ({
-            ...prevFormData,
-            [name]: null,
-          }));
-          setCcvError("Only numbers are allowed (maximum 3 digits)");
-        }
-        
-      } else if (name === 'cardnumber') {
+      const formattedCardName = value.replace(/\d/g, '').toUpperCase();
+     
 
-        const formattedCardNumber = value.replace(/\D/g, '').replace(/(.{4})/g, '$1 ');  // we can use .slice(0, 19) if we don't have maxLength={16} on input   
-        setFormData((prevFormData) => ({ 
+        setFormData((prevFormData: FormData) => ({ 
+        ...prevFormData,
+        [name]: formattedCardName,
+      }));
+      setDuplicatedFormData((prevDuplicatedFormData: FormData | null) => ({
+        ...prevDuplicatedFormData!,
+        [name]: formattedCardName,
+      }));
+   }
+    
+    
+    else if (name === 'validThru') {
+ 
+
+        const formattedValidThru = value.replace(/\D/g, '');
+    
+        setFormData((prevFormData: FormData) => ({
           ...prevFormData,
-           [name]: formattedCardNumber 
+          [name]: formattedValidThru,
         }));
-        setDuplicatedFormData({ ...formData, [name]: formattedCardNumber });
+    
+        setDuplicatedFormData((prevDuplicatedFormData: FormData | null) => ({
+          ...prevDuplicatedFormData!,
+          [name]: formattedValidThru,
+        }));
+      } 
+    
+    else if (name === 'ccv') {
+        
+        const formattedCcvNumber = value.replace(/\D/g, ''); ; //  or parseInt(value, 10)
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: formattedCcvNumber,
+        }));
+        setDuplicatedFormData((prevDuplicatedFormData: FormData | null) => ({
+          ...prevDuplicatedFormData!,
+          [name]: value,
+        })); 
+
       } else {
 
-      setFormData((prevFormData) => ({
+      setFormData((prevFormData: FormData) => ({
       ...prevFormData,
       [name]: value,
     }));
-  }
-  setDuplicatedFormData({ ...formData, [name]: value });
+ 
+  setDuplicatedFormData((prevDuplicatedFormData: FormData | null) => ({
+    ...prevDuplicatedFormData!,
+    [name]: value,
+  }));
+}
+  setFormErrors((prevFormErrors) => ({
+    ...prevFormErrors,
+    [name]: "",
+  }));
 }
 
 
@@ -88,9 +132,49 @@ const CardForm: React.FC<Props> = (props: Props) => {
 function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
   
   event.preventDefault();    // this prevents the default form submission behavior
+
+  if (formData.cardnumber.length !== 16) {
+    setFormErrors((prevFormErrors) => ({
+      ...prevFormErrors,
+      cardnumber: "Card number must be 16 digits",
+    }));
+    return;
+  }
+
+  if (formData.cardHolderName.length < 4) {
+    setFormErrors((prevFormErrors) => ({
+      ...prevFormErrors,
+      cardHolderName: "Firstname with lastname must be between 4 and 25 letters",
+    }));
+    return;
+  }
+
+  const validThruRegex = /^(0[1-9]|1[0-2])(2[2-9]|[3-9][0-9])$/;
+
+  if (!validThruRegex.test(formData.validThru)) {
+    setFormErrors((prevFormErrors) => ({
+      ...prevFormErrors,
+      validThru: "Use MMYY format with valid month and year",
+    }));
+    return;
+  }
+
+
+  if (formData.ccv.length !== 3) {
+    setFormErrors((prevFormErrors) => ({
+      ...prevFormErrors,
+      ccv: "CCV number must be 3 digits",
+    }));
+    return;
+  }
   
+  const numericCardNumber = Number(formData.cardnumber);
+  const numericCcv = Number(formData.ccv);
+
   const newForm = {
     ...formData,
+    cardnumber: numericCardNumber,
+    ccv: numericCcv,
     id: formId,
   };
 
@@ -111,10 +195,13 @@ function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
     cardnumber: "",
     cardHolderName: "",
     validThru: "",
-    ccv: null,
+    ccv: "",
   }));
 
    setFormId((prevFormId) => prevFormId + 1);
+   
+   setDuplicatedFormData(null);
+
 }
 
 /* function formatCardNumber(cardNumber: string): string {
@@ -147,8 +234,11 @@ function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
           id="cardnumber"
           value={formData.cardnumber}
           onChange={handleChange}
-          maxLength={19}      //maxLength attribute to limit input
+          maxLength={16}   //maxLength attribute to limit input
         />
+        {formErrors.cardnumber && (
+              <span className="error-message">{formErrors.cardnumber}</span>
+            )}
       </label>
       <label className="card-form__label" htmlFor="cardHolderName">
         Card Holder Name
@@ -159,7 +249,13 @@ function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
           id="cardHolderName"                 //  this id is used to connect the label and input
           value={formData.cardHolderName}
           onChange={handleChange}
+          placeholder="FIRSTNAME LASTNAME"
+          maxLength={25}
+          
         />
+        {formErrors.cardHolderName && (
+              <span className="error-message">{formErrors.cardHolderName}</span>
+            )}
       </label>
       <label className="card-form__label" htmlFor="validThru">
         Valid thru
@@ -170,8 +266,12 @@ function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
           id="validThru"
           value={formData.validThru}
           onChange={handleChange}
-          placeholder="MM/YY"
+          placeholder="MMYY"
+          maxLength={4} 
         />
+        {formErrors.validThru && (
+              <span className="error-message">{formErrors.validThru}</span>
+            )}
       </label>
       <label className="card-form__label" htmlFor="ccv">
         CCV
@@ -184,7 +284,9 @@ function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
           onChange={handleChange}
           maxLength={3} 
         />
-        {ccvError && <span className="error-message">{ccvError}</span>}
+         {formErrors.ccv && (
+              <span className="error-message">{formErrors.ccv}</span>
+            )}
       </label>
       <button className="card-form__submit" type="submit">
         Submit this!
